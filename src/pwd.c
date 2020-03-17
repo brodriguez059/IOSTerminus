c24199c58cb15b9fcfe8b2e5804b4a2f6a968756
alexbcberio <alexbcberio@gmail.com>
2020-03-17 14:49:13 +0100

    Handled correctly errors + code clean

--- src/pwd.c
+++ src/pwd.c
@@ -1,55 +1,58 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#define BUFFER_SIZE 128
+// import PATH_MAX (max path length)
+#include <linux/limits.h>
 #define STDOUT 1
+#define STDERR 2
 
-int getCurrentWorkingDirectory(char *buf) {
-  if (getcwd(buf, BUFFER_SIZE) == NULL) {
+int getCurrentWorkingDirectory(char *buf, size_t size) {
+    if (getcwd(buf, size) == NULL) {
         char *error;
         switch(errno) {
             case EACCES:
-                error = "Permission to read denied.\n";
-                break;
+            error = "Permission to read denied.";
+            break;
             case EFAULT:
-                error = "Bad address.\n";
-                break;
+            error = "Bad address.";
+            break;
             case EINVAL:
-                error = "Buffer is not a null pointer or size is zero.\n";
-                break;
+            error = "Buffer is not a null pointer or size is zero.";
+            break;
             case ENAMETOOLONG:
-                error = "Pathname string exceeds PATH_MAX bytes.\n";
-                break;
+            error = "Pathname string exceeds PATH_MAX bytes.";
+            break;
             case ENOENT:
-                error = "Directory was unlinked.\n";
-                break;
+            error = "Directory was unlinked.";
+            break;
             case ENOMEM:
-                error = "Out of memory.\n";
-                break;
+            error = "Out of memory.";
+            break;
             case ERANGE:
-                error = "Size argument is less than the length of the pathname.\n";
-                break;
+            error = "Size argument is less than the length of the pathname.";
+            break;
             default:
-                error = "Unknown error.\n";
+            error = "Unknown error.";
         }
-    write(STDOUT, error, strlen(error));
-    return 1;
-
-  } else {
-    return 0;
-  }
+        strcpy(buf, error);
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 int main(int argc, char *argv[]) {
-  char cwd[BUFFER_SIZE];
-  int rCode = getCurrentWorkingDirectory(cwd);
-  if (rCode == 0) {
-    char out[strlen(cwd) + 2];
+    char cwd[PATH_MAX];
+    int rCode = getCurrentWorkingDirectory(cwd, sizeof(cwd));
+    char out[strlen(cwd)];
     strcpy(out, cwd);
     strcat(out, "\n");
-    write(STDOUT, out, strlen(out));
-  } else {
-    // TOOD: manage error
-  }
-}
 
+    if (rCode == 0) {
+        write(STDOUT, out, strlen(out));
+    } else {
+        char *prefix = "[ERROR] ";
+        write(STDERR, prefix, strlen(prefix));
+        write(STDERR, out, strlen(out));
+    }
+}
