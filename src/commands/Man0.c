4af0f1d4b6e420363068b843f90653eed436f77d
Jon <jonrubio1999@gmail.com>
2020-05-14 16:09:25 +0200

    New man command

---
+++ src/commands/Man0.c
@@ -0,0 +1,29 @@
+#include "terminuslib.h"
+int game_state;
+
+int main (int argc, char *argv[]) {
+
+int fd;
+char c;
+int nArg:
+	
+	fifo_read(&nArg, &game_state, gameDirs);
+
+	 
+    char path[512];
+
+    strcpy(path, gameDirs[DOCS].name)
+    strcat(path, argv[1]);
+    strcat(path,".txt");
+
+    fd= open(path, O_RDONLY);
+
+    if(fd < 0) {
+		error("There was an error opening the file")
+	}
+
+	while(read(fd, &c, 1)>0{
+		if(write(STDOUT, &c, 1) < 1){
+			error("There was an error reading the file")
+		}
+}
